2ce541bc8a701654b01c9af9a30f6a9bcac56df0
Alex Hossien <alexhossien@gmail.com>
2024-08-26 20:49:41 +0100

    Testing narrowring conversion with empty list initialisation with cin

--- ch1/1.4_VariableAssigment&Initialisation.cpp
+++ ch1/1.4_VariableAssigment&Initialisation.cpp
@@ -8,7 +8,7 @@ Basics of variables: assignment and initialisation
 
 // Declare a variable called x that can hold an integer
 // Can be done outside of a function
-int x;
+int x{};
 //x = 2;  Error: This is not allowed outside of a function
 
 int main()
@@ -18,13 +18,16 @@ int main()
 
     int depth { 5 }; // Direct list Initialisation
     int height = { 5 }; // Copy list Initialisation
-    int weight{}; // Initialiser is empty (weight is 0) (default initialisation)
+    int weight{}; // Initialiser is empty (weight is 0, or empty for other types) (default initialisation)
 
     std::cout << width << " : width \n";
     std::cout << depth << " : depth \n";
     std::cout << height << " : height \n";
     std::cout << weight << " : weight \n";
     
+    std::cin >> x;
+    std::cout << x << " : x \n";
+
     return 0;
 
 }
